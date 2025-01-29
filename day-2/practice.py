1942765d0083034eaa5a6145fc5178985717c732
arsalan4work <you@example.com>
2025-01-29 17:00:44 +0500

    python learning day 2

---
+++ day-2/practice.py
@@ -0,0 +1,42 @@
+# Write a program to input user's name % print its length.
+
+# firstName = input("Enter your first name: ")
+# lastName = input("Enter your last name: ")
+# print(firstName + " " + lastName)
+# print(len(firstName))
+
+# Write a program to find occurrence of '$' in a string.
+
+# name = "$hi my $name is Ar$alan"
+# print(name.count("$"))
+
+# Write a program to check if a number entered by the user is odd or even.
+
+# num = int(input("Enter a number: "))
+
+# rem = num % 2
+# if (rem == 0):
+#    print("Even")
+# else:
+#    print("Odd")
+
+# Write a program to find the greatest of 3 numberss entered by the user.
+
+# a = int(input("Enter number 1: "))
+# b = int(input("Enter number 2: "))
+# c = int(input("Enter number 3: "))
+
+# if(a >= b and a >= c):
+#    print("Frist number is largest!", a)
+# elif(b >= c):
+#    print("Second number is largest!", b)
+# else:
+#    print("Third number is largest!", c)
+
+# 
+x = int(input("Enter Number: "))
+
+if(x % 5 == 0):
+   print("Multiple of 5")
+else:
+   print("Not a multiple")
---
+++ day-2/stringFunctions.py
@@ -0,0 +1,7 @@
+# String Functions
+str1 = "i am Arsalan a Python Developer"
+print(str1.endswith("er")) # Returns true if string ends with substr 
+print(str1.capitalize()) # Capatalize 1st char 
+print(str1.replace("a","o")) # Replaces all occurrences of old value to new value
+print(str1.find("a")) # Returns 1st index of 1st occurrer
+print(str1.count("a")) # Counts the occurrence of substr
