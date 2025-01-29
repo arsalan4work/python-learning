a55e06a0f8d35313eaae54e83de280b2f527a6e0
arsalan4work <you@example.com>
2025-01-29 13:14:24 +0500

    python learning day 1

---
+++ day-1/practice.py
@@ -0,0 +1,23 @@
+# Write a program to input 2 numbers & print their sum.
+# sum1 = int(input("Enter the first number: "))
+# sum2 = int(input("Enter the second number: "))
+# sum3 = sum1 + sum2
+# print("Your total sum is: ", sum3)
+
+# Write a program that input side of a square & print it's area.
+# side = int(input("Enter a number: "))
+# area = side * side
+# print("Total area of square is: ",area)
+
+# Write a program that input 2 floating point numbers & print their average.
+# input1 = float(input("Enter floating value 1: "))
+# input2 = float(input("Enter floating value 2: "))
+# ans = input1 + input2 / 2
+# print("The average is: ", ans)
+
+# Write a program that input 2 int numbers, a and b. Print True if a is greater than b or equal to b. If not print False
+# a = int(input("Enter the first number: "))
+# b = int(input("Enter the second number: "))
+
+# print("Answer is: ", (a >= b) or (a == b))
+
