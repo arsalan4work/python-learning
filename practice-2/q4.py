b33fe3238b8707accc2373bac8325ff7fb4483f8
arsalan4work <you@example.com>
2025-02-06 20:28:51 +0500

    add files

---
+++ practice-2/q4.py
@@ -0,0 +1,11 @@
+# Q4: Write a Python function that checks whether a given string 
+# is a palindrome (reads the same forwards and backwards) while ignoring spaces and punctuation.
+
+
+import re  
+
+def is_palindrome(s):
+    clean_s = re.sub(r'["^a-zA-Z0-9"]', '', s).lower()  # Remove non-alphanumeric characters
+    return clean_s == clean_s[::-1]  # Compare with reverse
+
+print(is_palindrome("A man, a plan, a canal: Panama"))  # Output: True
