1effd2380c4cf1ba845e26782c121ad587ed49d5
arsalan4work <m.arsalan4210@gmail.com>
2025-02-12 14:01:00 +0500

    Lecture 2

--- Learning-2/lecture-2/ch-02.py
+++ Learning-2/lecture-2/ch-02.py
@@ -1,1 +1,75 @@
-print("hello words")
+# # Airthematic Operators
+# a = 1
+# b = 2
+# print(a + b) 
+# print(a - b) 
+# print(a * b) 
+# print(a / b) 
+
+# # Assignment Operators
+# a = 2
+# a += 3
+# print(a)
+# a -= 2
+# print(a)
+# a *= 2
+# print(a)
+# a /= 4
+# print(a)
+
+# # Comparison Operators
+# a = 2<1
+# print(a)
+# a = 2>1
+# print(a)
+# a = 2<=1
+# print(a)
+# a = 2>=1
+# print(a)
+# a = 2!=1
+# print(a)
+# a = 2==1
+# print(a)
+
+
+# # Logical Operators and, or, not
+
+# e = True or False
+# # Truth table of 'or'
+# print("Ture or False is", True or False)
+# print("Ture or True is", True or True)
+# print("False or True is", False or True)
+# print("False or False is", False or False)
+
+# # Truth table of 'and'
+# print("Ture and False is", True and False)
+# print("Ture and True is", True and True)
+# print("False and True is", False and True)
+# print("False and False is", False and False)
+
+# # Truth table of 'not'
+# print(not(True))
+
+
+# Type() func and typecasting
+# a = 31
+# print(type(a))
+
+# b = "Arsalan"
+# print(type(b))
+
+# c = 31.22
+# print(type(c))
+
+# d = False
+# print(type(d))
+
+# a = "32.11"
+# b = float(a)
+# print(type(b))
+
+
+# Input Func
+
+# user = input("Enter a number: ")
+# print(user)
---
+++ Learning-2/lecture-2/practice-02.py
@@ -0,0 +1,34 @@
+# 1. Write a python program to add two numbers.
+# a = 2
+# b = 2
+# print(a + b)
+
+
+# 2. Write a python program to find remainder when a number is divided by z.
+# a = 3
+# b = 2
+# z = (a % b)
+# print(z)
+
+
+# 3. Check the type of variable assigned using input () function.
+# a = input("Enter something: ")
+# print(type(a))
+
+
+# 4. Use comparison operator to find out whether ‘a’ given variable a is greater than
+#    ‘b’ or not. Take a = 34 and b = 80
+# a = 34
+# b = 80
+# print(a > b)
+
+
+# 5. Write a python program to find an average of two numbers entered by the user.
+# inp1 = int(input("Enter number 1: "))
+# inp2 = int(input("Enter number 2: "))
+# print((inp1 + inp2) / 2)
+
+# 6. Write a python program to calculate the square of a number entered by the user.
+
+# a = int(input("Enter number 1: "))
+# print(a ** 2)
