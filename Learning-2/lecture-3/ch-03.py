c0984900613d7d99d428a6155a74813a9798b9b5
arsalan4work <m.arsalan4210@gmail.com>
2025-02-12 15:52:35 +0500

    Lecture 4

---
+++ Learning-2/lecture-3/ch-03.py
@@ -0,0 +1,50 @@
+# Strings
+name = "arsalan"
+# name_length = len(name)
+# print(name_length)
+
+# # Indexing
+# name_short = name[3]
+# print(name_short)
+
+# # Slicing
+# name_slicing = name[1:5] # Starts from 1 all the way till 4 (excluding 5)
+# print(name_slicing)
+
+# # Reverse Slicing or negative slicing
+# name_r_slicing = name[::-1] # Starts from -1 all the way till len
+# print(name_r_slicing)
+
+# # Slicing by steps
+# name_s_slicing = name[0:5:2] # Starts from 0 all the way till 5 (Skip 2 steps or words)
+# print(name_s_slicing) 
+
+
+# String Functions
+# String length()
+# name_length = len(name)
+# print(name_length)
+
+# String Endswith("")
+# name1 = name.endswith("sala")
+# print(name1)
+
+# String Startwith("")
+# name2 = name.startswith("Ars")
+# print(name2)
+
+# String count("")
+# name3 = name.count("a")
+# print(name3)
+
+# String Capatalize (Capatilize first word of the sentence)
+# name4 = name.capitalize()
+# print(name4)
+
+# # String Find("")
+# name5 = name.find("ars")
+# print(name5)
+
+# String Replace (old word, new word)
+# name6 = name.replace("ars", "ass")
+# print(name6)
---
+++ Learning-2/lecture-3/practice-03.py
@@ -0,0 +1,30 @@
+# 1. Write a python program to display a user entered name followed by Good
+# Afternoon using input () function.
+# user = input("Enter your name: ")
+# print(f"Good Afternoon {user}")
+
+
+# 2. Write a program to fill in a letter template given below with name and date.
+# letter = '''
+# Dear <|Name|>,
+# You are selected!
+# <|Date|>
+# '''
+# print(letter.replace("<|Name|>", "Arsalan").replace("<|Date|>", "12.2.2025"))
+
+
+
+# 3. Write a program to detect double space in a string.
+# lst = "Arsalan  Arsalan"
+# print(lst.find("  "))
+
+
+# 4. Replace the double space from problem 3 with single spaces.
+# list_replace = lst.replace("  ", " ")
+# print(list_replace)
+
+
+# 5. Write a program to format the following letter using escape sequence
+# characters. letter = "Dear Harry, this python course is nice. Thanks!"
+# letter = "Dear Arsalan,\nthis python course is nice.\nThanks!"
+# print(letter)
