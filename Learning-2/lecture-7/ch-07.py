b3599d3df314c87c0d25b9ba90abffd142713903
arsalan4work <m.arsalan4210@gmail.com>
2025-02-14 12:50:43 +0500

    Lecture 7

---
+++ Learning-2/lecture-7/ch-07.py
@@ -0,0 +1,75 @@
+# Loops 
+# For loop: A for loop is used to iterate through a sequence like list, tuple, or string [iterables] 
+
+# for i in range(1,6): #specify range(start, stop, step_size)
+#    print(i)
+
+
+# l = [1, "Arsalan", False, "Bilal", "Ahmed"]
+# i = 0
+# while(i<len(l)):
+#    print(l[i])
+#    i += 1
+
+# For loop with list
+# l1 = [1,4,6,7,8,9,0]
+# for i in l1:
+#    print(i)
+
+# For loop with Tuple
+# t = (6,2,3,4,5,6,7,8)
+# for i in t:
+#    print(i)
+
+# For loop with String
+# name = "Arsalan"
+# for i in name:
+#    print(i)
+
+# l = [1,7,8]
+# for item in l:
+#    print(item)
+# else:
+#    print("Done")
+
+# for i in range(1,100):
+#    print(i)
+#    if i == 5:
+#       break #exit the loop
+
+# l = ["Arsalan", "Ali", "Ahmed", "Bilal", "Hassan", "Zohaib"]
+# for i in l:
+#    if i == "Ahmed":
+#       continue #skip this iteration
+#    print(i)
+
+# dic = {
+#    "name1": "Arsalan",
+#    "name2": "Ahmed",
+#    "name3": "Ali",
+#    "name4": "Hassan",
+# }
+
+# # for i in dic:
+# #    if dic[i] == "Ali":
+# #       continue #skip this iteration
+# #    print(i)
+
+# for i in dic:
+#    if dic[i] == "Ali":
+#       break 
+#    print(i)
+
+# l = [1,2,3,4]
+# for i in l:
+#    pass # pass is a null statement in python.
+
+########################################################################################################################################################
+
+# # While loop: In while loops, the condition is checked first. If it evaluates to true, the body of the loop
+# is executed otherwise not!
+
+# i = 0
+# while i < 5111111:
+#     print("Arsalan")
+#     i += 1
